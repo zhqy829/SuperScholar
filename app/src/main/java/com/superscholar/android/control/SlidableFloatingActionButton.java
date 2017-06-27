0a7379b293cfef8c4696caaf0678addf04ef6734
zhqydot <603253903@qq.com>
2017-06-27 22:36:23 +0800

    更新可拖动的浮动按钮

--- app/src/main/java/com/superscholar/android/control/SlidableFloatingActionButton.java
+++ app/src/main/java/com/superscholar/android/control/SlidableFloatingActionButton.java
@@ -1,7 +1,9 @@
 package com.superscholar.android.control;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -17,28 +19,37 @@ public class SlidableFloatingActionButton extends FloatingActionButton implement
     private boolean isMove;
     private int lastX;
     private int lastY;
+    int screenWidth;
+    int screenHeight;
     private long startTime = 0;
     private long endTime = 0;
 
     public SlidableFloatingActionButton(Context context) {
         this(context, null);
         this.setOnTouchListener(this);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels - 50;
     }
 
     public SlidableFloatingActionButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.setOnTouchListener(this);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels - 50;
     }
 
     public SlidableFloatingActionButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         this.setOnTouchListener(this);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels - 50;
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        int x=(int)motionEvent.getRawX();
-        int y=(int)motionEvent.getRawY();
         if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
             isMove=false;
             startTime = System.currentTimeMillis();
@@ -46,23 +57,42 @@ public class SlidableFloatingActionButton extends FloatingActionButton implement
             lastY=(int)motionEvent.getRawY();
         } else if(motionEvent.getAction()==MotionEvent.ACTION_MOVE){
             isMove=true;
-            int deltaX=x-lastX;
-            int deltaY=y-lastY;
-            int translationX=(int)this.getTranslationX()+deltaX;
-            int translationY=(int)this.getTranslationY()+deltaY;
-            this.setTranslationX(translationX);
-            this.setTranslationY(translationY);
+            int dx =(int)motionEvent.getRawX() - lastX;
+            int dy =(int)motionEvent.getRawY() - lastY;
+
+            int left = view.getLeft() + dx;
+            int top = view.getTop() + dy;
+            int right = view.getRight() + dx;
+            int bottom = view.getBottom() + dy;
+            if(left < 0){
+                left = 0;
+                right = left + view.getWidth();
+            }
+            if(right > screenWidth){
+                right = screenWidth;
+                left = right - view.getWidth();
+            }
+            if(top < 0){
+                top = 0;
+                bottom = top + view.getHeight();
+            }
+            if(bottom > screenHeight-150){
+                bottom = screenHeight-150;
+                top = bottom - view.getHeight();
+            }
+            view.layout(left, top, right, bottom);
+
         }else if(motionEvent.getAction()==MotionEvent.ACTION_UP){
             endTime = System.currentTimeMillis();
-            if ((endTime - startTime) > 0.1 * 1000L) {
+            if ((endTime - startTime) > 0.2 * 1000L) {
                 isMove = true;
             } else {
                 isMove = false;
             }
         }
-        lastX=x;
-        lastY=y;
-        this.setPressed(!isMove);
+        lastX = (int) motionEvent.getRawX();
+        lastY = (int) motionEvent.getRawY();
+        this.setPressed(false);
         return isMove;
     }
 
